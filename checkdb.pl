128ae9339a0f4ff4bbd893fe281f7392b01cf5ca
Kuenstlah <Kevin@Kuenstlah.de>
2016-09-16 10:12:56 +0200

    Update

--- checkdb.pl
+++ checkdb.pl
@@ -17,19 +17,46 @@ my $pokemon_ids;
 my $empf = shift || 'Debug';
 my $mysql_db="pogo";
 
+if ($mode eq "spawn_hilden") {
+	$mysql_db="pogo";
+	$mode = "spawn";
+} elsif ($mode eq "spawn_america") {
+	$mysql_db="pogo_america";
+	$mode = "spawn";
+} elsif ($mode eq "spawn_garath") {
+	$mysql_db="pogo_garath";
+	$mode = "spawn";	
+}
+if ($mode eq "spawn") {
+	$pokemon_ids=	"1,2,3,4,5,6,7,8,9,12,15,18,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,42,44,45,47,49,50,51,52,53,55,56,57,58,59,61,62,63,64,65,66,67,68,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,91,92,93,94,95,97,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,117,119,121,122,123,125,126,127,128,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151";
+}
 if ($mode eq "ticker_hilden") {
 	$empf="PoGo-Ticker";
 	$mysql_db="pogo";
 	$mode = "ticker";
+	if (($hour >= 0 && $hour <= 6) && ( $day==1 || $day==2 || $day==3 || $day==4 || $day==5) ) {
+		# Ticker shows only rare Pokemon during night and at work days
+		$pokemon_ids="3,6,9,45,59,62,65,68,71,76,78,83,103,112,115,128,130,131,132,134,135,136,139,141,142,144,145,146,149,150,151";
+	} else {
+		$pokemon_ids="3,6,9,24,26,28,31,34,36,38,40,45,51,53,57,59,62,65,68,71,76,78,80,82,83,85,87,89,91,94,95,101,103,106,107,108,110,112,113,114,115,123,125,127,128,130,131,132,134,135,136,137,139,141,142,143,144,145,146,148,149,150,151";
+	}
 } elsif ($mode eq "ticker_america") {
-        $empf="PoGo-Ticker-America";
-        $mysql_db="pogo_america";
-        $mode = "ticker";
+	$empf="PoGo-Ticker-America";
+	$mysql_db="pogo_america";
+	$mode = "ticker";
+	$pokemon_ids="3,6,9,24,26,28,31,34,36,38,40,45,51,53,57,59,62,65,68,71,76,78,80,82,83,85,87,89,91,94,95,101,103,106,107,108,110,112,113,115,125,126,130,131,132,134,135,136,137,139,141,142,143,144,145,146,148,149,150,151";
 } elsif ($mode eq "ticker_garath") {
-        $empf="PoGo-Ticker-Garath";
-        $mysql_db="pogo_garath";
-        $mode = "ticker";
+	$empf="PoGo-Ticker-Garath";
+	$mysql_db="pogo_garath";
+	$mode = "ticker";
+	if (($hour >= 0 && $hour <= 6) && ( $day==1 || $day==2 || $day==3 || $day==4 || $day==5) ) {
+		# Ticker shows only rare Pokemon during night and at work days
+		$pokemon_ids="3,6,9,45,59,62,65,68,71,76,78,83,103,112,115,128,130,131,132,134,135,136,139,141,142,144,145,146,149,150,151";
+	} else {
+		$pokemon_ids="3,6,9,24,26,28,31,34,36,38,40,45,51,53,57,59,62,65,68,71,76,78,80,82,83,85,87,89,91,94,95,101,103,106,107,108,110,112,113,114,115,123,125,127,128,130,131,132,134,135,136,137,139,141,142,143,144,145,146,148,149,150,151";
+	}
 }
+
 sub Debug{
         if ($debug || $empf eq "Debug") {
                 my ( $text ) = @_;
@@ -55,17 +82,6 @@ sub TGpos{
         }
 }
 
-if ($mode eq "spawn") {
-	$pokemon_ids=	"1,2,3,4,5,6,7,8,9,12,15,18,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,42,44,45,47,49,50,51,52,53,55,56,57,58,59,61,62,63,64,65,66,67,68,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,91,92,93,94,95,97,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,117,119,121,122,123,125,126,127,128,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151";
-} elsif ($mode eq "ticker") {
-	if (($hour >= 0 && $hour <= 6) && ( $day==1 || $day==2 || $day==3 || $day==4 || $day==5) ) {
-		# Ticker shows only rare Pokemon during night and at work days
-		$pokemon_ids="3,6,9,45,59,62,65,68,71,76,78,83,103,112,115,128,130,131,132,134,135,136,139,141,142,144,145,146,149,150,151";
-	} else {
-		$pokemon_ids="3,6,9,24,26,28,31,34,36,38,40,45,51,53,57,59,62,65,68,71,76,78,80,82,83,85,87,89,91,94,95,101,103,106,107,108,110,112,113,114,115,123,125,127,128,130,131,132,134,135,136,137,139,141,142,143,144,145,146,148,149,150,151";
-	}
-}
-
 # http://pokewiki.de/Pok%C3%A9mon-Liste
 if (!$pokemon_ids) {
 	print "Wrong string <pokemon_ids>? Cancel.";
@@ -86,7 +102,9 @@ my @row;
 if ($mysql) {
         $db = DBI->connect("DBI:mysql:$mysql_db:localhost","root","root") or die "Connection Error: $DBI::errstr\n";
 } else {
-        $db = DBI->connect("DBI:SQLite:/home/pi/PokemonGo-Map/pogom.db", "", "",{RaiseError => 1, AutoCommit => 1});
+        #$db = DBI->connect("DBI:SQLite:/home/pi/PokemonGo-Map/pogom.db", "", "",{RaiseError => 1, AutoCommit => 1});
+		print "######## ERROR: Cannot use sqlite ####";
+		exit;
 }
 $dt_now->subtract(minutes => 20);
 
