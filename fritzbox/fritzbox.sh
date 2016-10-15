235508979f9eb9d67d46b87217fc42c44f41f734
Kuenstlah <KevinKuenstler@hotmail.de>
2016-10-15 18:07:16 +0200

    Fixed paths

--- fritzbox/fritzbox.sh
+++ fritzbox/fritzbox.sh
@@ -11,11 +11,11 @@ date_hm=`date "+%d.%m. %H:%M"`
 
 if [[ $1 == 'status' ]];then
 	echo '<?xml version="1.0" encoding="utf-8"?><fritzxml>' > $xmlfile
-	curl -s "http://fritz.box:49000/igdupnp/control/WANIPConn1" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANIPConnection:1#GetStatusInfo" -d "@/share/Web/scripts/connection_state.xml" | egrep "New" >> $xmlfile
+	curl -s "http://fritz.box:49000/igdupnp/control/WANIPConn1" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANIPConnection:1#GetStatusInfo" -d "@/share/Web/scripts/fritzbox/connection_state.xml" | egrep "New" >> $xmlfile
 	echo '</fritzxml>' >> $xmlfile
 elif [[ $1 == 'linkspeed' ]];then
 	echo '<?xml version="1.0" encoding="utf-8"?><fritzxml>' > $xmlfile
-	curl -s "http://fritz.box:49000/igdupnp/control/WANCommonIFC1" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1#GetAddonInfos" -d "@/share/Web/scripts/linkspeed.xml" | egrep "New" >> $xmlfile
+	curl -s "http://fritz.box:49000/igdupnp/control/WANCommonIFC1" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1#GetAddonInfos" -d "@/share/Web/scripts/fritzbox/linkspeed.xml" | egrep "New" >> $xmlfile
 	echo '</fritzxml>' >> $xmlfile
 	current_rows=`wc -l $xmlfile.chart.csv| awk -F ' ' {'print $1'}`
 	if [[ $current_rows -gt 150 ]];then
