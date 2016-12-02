a365bbcb92088f4961cae74a533c3a02f2a3b5e2
Kuenstlah <Kevin@Kuenstlah.de>
2016-12-02 21:14:38 +0100

    Added database settings

--- pogo/start.sh
+++ pogo/start.sh
@@ -4,26 +4,29 @@
 #######################
 
 # Get $captcha_api
-cfg=`echo $0|cut -d '.' -f1`.cfg
-[ -f "$cfg" ] && source "$cfg" || echo "Error - config <$cfg> is missing. Do not use additional . in path to call the script, except for ending."
+cfg=`/bin/echo $0|cut -d '.' -f1`.cfg
+[ -f "$cfg" ] && source "$cfg" || /bin/echo "Error - config <$cfg> is missing. Do not use additional . in path to call the script, except for ending."
 
 cd $path
 if [[ update_gitrepos -eq 1 ]];then
-	echo "Git update.."
-	for i in `ls -1 $path|grep "pogomap_"`;do echo "##### $i";cd $path/$i;git pull;done
+	/bin/echo "Git update.."
+	used=`grep start_map /home/pi/scripts/pogo/start.cfg |grep -v "#" |awk -F ' ' {'print $2'}|tr '\n' ' '`
+	for i in $used;do /bin/echo "##### $i";cd $path/$i;/usr/bin/git pull;done
 fi
 
 function start_map(){
 	map="$1"
 	port=$2
-	loc="$3"
-	ps aux|grep -v grep|egrep -w "$loc|$path/$map" > /dev/null 2>&1
+	steps="$3"
+	db="$4"
+	loc="$5"
+	/bin/ps aux|grep -v grep|egrep -w "$loc|$path/$map" > /dev/null 2>&1
 	if [[ $? -eq 1 ]];then
-		echo -e "Map <$map>:\t\tStarting"
-		[ $captcha -eq 1 ] && screen -d -S $map -m bash -c "$python_bin $path/$map/runserver.py --location '$2' --port $port --host $host  --locale de -cs -ck $captcha_api " \
-				|| screen -d -S $map -m bash -c "$python_bin $path/$map/runserver.py --location '$2' --port $port --host $host  --locale de"
+		/bin/echo -e "Map <$map>:\t\tStarting"
+		[ $captcha -eq 1 ] && /usr/bin/screen -d -S $map -m /bin/bash -c "$python_bin $path/$map/runserver.py --location '$loc' --port $port --host $host --db-type $db_type  --db-name $db --db-user $db_user --db-pass $db_pw  --db-host $db_host --db-port $db_port --gmaps-key $gmaps_key --status-name $map --step-limit $steps --locale de -cs -ck $captcha_api" \
+				|| /usr/bin/screen -d -S $map -m /bin/bash -c "$python_bin $path/$map/runserver.py --location '$loc' --port $port --host $host  --locale de"
 	else
-		echo -e "Map <$map>:\t\tAlready running"
+		/bin/echo -e "Map <$map>:\t\tAlready running"
 	fi
 
 }
