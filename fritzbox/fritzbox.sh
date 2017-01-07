6fb02de36b8a8fc582dc5f6455999349482f4b79
Kuenstlah <kevinkuenstler@hotmail.de>
2017-01-07 15:15:46 +0100

    Updated csv creation

--- fritzbox/fritzbox.sh
+++ fritzbox/fritzbox.sh
@@ -7,7 +7,7 @@ pwd=`pwd`
 xmlpath=/share/Web/xml/
 xmlfile="$xmlpath/fritz_$1.xml"
 
-date_hm=`date "+%d.%m. %H:%M"`
+date_hm=`date "+%m-%d-%Y %H:%M"`
 
 if [[ $1 == 'status' ]];then
 	echo '<?xml version="1.0" encoding="utf-8"?><fritzxml>' > $xmlfile
@@ -18,17 +18,22 @@ elif [[ $1 == 'linkspeed' ]];then
 	curl -s "http://fritz.box:49000/igdupnp/control/WANCommonIFC1" -H "Content-Type: text/xml; charset="utf-8"" -H "SoapAction:urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1#GetAddonInfos" -d "@/share/Web/scripts/fritzbox/linkspeed.xml" | egrep "New" >> $xmlfile
 	echo '</fritzxml>' >> $xmlfile
 	current_rows=`wc -l $xmlfile.chart.csv| awk -F ' ' {'print $1'}`
-	if [[ $current_rows -gt 150 ]];then
+	if [[ $current_rows -ge 70 ]];then
 		/bin/sed 2D -i $xmlfile.chart.csv
 	fi
 	sendrate=`awk -F "[><]" '/NewByteSendRate/{print $3}' $xmlfile`
 	receiverate=`awk -F "[><]" '/NewByteReceiveRate/{print $3}' $xmlfile`
 	sendrate_new="$(($sendrate/1024/1024))"
 	receiverate_new="$(($receiverate/1024/1024))"
-	grep -q "$date_hm," $xmlfile.chart.csv
-	if [[ $? == "1" ]];then 
-		echo -e "$date_hm,$receiverate_new,$sendrate_new" >> $xmlfile.chart.csv
+	if [[ $sendrate_new == "0" ]] && [[ $receiverate_new == "0" ]];then 
+		exit
+	else
+		grep -q "$date_hm," $xmlfile.chart.csv
+		if [[ $? == "1" ]];then 
+			echo -e "$date_hm,$receiverate_new,$sendrate_new" >> $xmlfile.chart.csv
+		fi
 	fi
+
 else
 	echo "Wrong parameter. Exiting."
 	exit;
