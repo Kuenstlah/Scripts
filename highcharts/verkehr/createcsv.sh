cea7bc9e80bc42fb30fbca583da487aa1109d2bf
Kuenstlah <kevinkuenstler@hotmail.de>
2017-01-07 17:24:59 +0100

    Updated traffic script

--- highcharts/verkehr/createcsv.sh
+++ highcharts/verkehr/createcsv.sh
@@ -5,29 +5,36 @@
 cfg=`/bin/echo $0|cut -d '.' -f1`.cfg
 [ -f "$cfg" ] && source "$cfg" || /bin/echo "Error - config <$cfg> is missing. Do not use additional . in path to call the script, except for ending."
 
+csvline="$date_hm"
+
 function createcsv(){
         name=$1
         locstart=$2
         locdest=$3
 
-        csvfile="$xmlpath/verkehr_${name}.csv"
-        tmp_json_file=/tmp/json_${name}.txt
-        curl -s "https://maps.googleapis.com/maps/api/directions/json?origin=${locstart}&destination=${locdest}&key=$google_api_key" > $tmp_json_file
-
-
-        distance=($(jq -r '.routes[0].legs[0].distance.value' $tmp_json_file))
+        csvfile="$xmlpath/traffic.csv"
+        tmp_json_file="$csvfile.tmp"
+        curl --insecure "https://maps.googleapis.com/maps/api/directions/json?origin=${locstart}&destination=${locdest}&key=$google_api_key" > $tmp_json_file
+	cat $tmp_json_file
         duration=($(jq -r '.routes[0].legs[0].duration.value' $tmp_json_file))
-
-        if [[ $distance == "0" ]] && [[ $duration == "0" ]];then
+	duration_new="$(($duration/60))"
+	if [[ $duration_new -gt 0 ]];then
+                csvline="$csvline,$duration_new"
+	else
+                echo "$date_hm - duration_new: $duration_new - duration: $duration" >> ${csvfile}.log
                 exit
-        else
-                grep -q "$date_hm,$distance,$duration" $csvfile
-                if [[ $? == "1" ]];then
-                        echo -e "$date_hm,$distance,$duration" >> $csvfile
-                fi
-        fi
-
-        rm -f $tmp_json_file
+	fi
 }
-
 call_jobs
+
+current_rows=`wc -l $csvfile| awk -F ' ' {'print $1'}`
+if [[ $current_rows -ge 50 ]];then
+	/bin/sed 2D -i $csvfile
+fi
+
+grep -q "$csvline" $csvfile
+if [[ $? == "1" ]];then
+	echo -e "$csvline" >> $csvfile
+fi
+
+rm -f $tmp_json_file
